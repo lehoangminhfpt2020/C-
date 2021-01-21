6c672fdb5e120859096d963012b475669451c2ae
minh <65264453+lehoangminhfpt2020@users.noreply.github.com>
2021-01-21 22:34:58 +0700

    Lad9

---
+++ Lad9/CSDBC/EFCore/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using EFCore.Models;
+
+namespace EFCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+            insertProduct();
+
+            Console.WriteLine("press any key to continue");
+            Console.ReadKey();
+        }
+
+        static void insertProduct()
+        {
+            using (var db = new EFContext())
+            {
+                Product product = new Product();
+                product.Name = "Pen Drive";
+                db.Add(product);
+
+                product = new Product();
+                product.Name = "Memory Card";
+                db.Add(product);
+
+                db.SaveChanges();
+            }
+            return;
+        }
+    }
+
+}
