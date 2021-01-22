a8ed58340893fc9178851b7693657b23ebfcce98
minh <65264453+lehoangminhfpt2020@users.noreply.github.com>
2021-01-22 09:32:59 +0700

    Practice

---
+++ GeometryExample/Exercise2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Exercise2
+{
+    class Baseclass
+    {
+        String name;
+        int weight;
+        public void SetMe(int weight,string name)
+        {
+            this.name = name;
+            this.weight = weight;
+        }
+        public void Show()
+        {
+            Console.WriteLine("name: {0} , Weight {1}", name, weight);
+        }
+    }
+
+    class lion : Baseclass
+    {
+       
+    }
+
+    class Tiger : Baseclass
+    {
+
+    }
+   class zoo
+    {
+        static void Main()
+        {
+
+            lion lion1 = new lion();
+            lion1.SetMe(200, "Lion");
+            lion1.Show();
+
+            Tiger Tiger1 = new Tiger();
+            Tiger1.SetMe(100, "tiger");
+            Tiger1.Show();
+
+
+
+        }
+    }
+}
+
+
---
+++ GeometryExample/GeometryExample/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace GeometryExample
+{
+    class Cylinder
+    {
+        public double Radius;
+        public double Height;
+        public double BaseArea;
+        public double LateralArea;
+        public double TotalArea;
+        public double Volume;
+
+        public static void Main()
+        {
+            Cylinder obj = new Cylinder();
+            obj.Process();
+            obj.Result();
+        }
+         void Process()
+        {
+            Console.WriteLine("Enter the dimenstions of the Cylinder: ");
+            Console.WriteLine("Radius: ");
+            Radius = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Height: ");
+            Height = Convert.ToDouble(Console.ReadLine());
+
+            BaseArea = Radius * Radius * Math.PI;
+            LateralArea = 2 * Math.PI * Radius * Height;
+            TotalArea = 2 * Math.PI * Radius * (Height + Radius);
+            Volume = Math.PI * Radius * Radius * Height;
+        }
+
+        void Result()
+        {
+            Console.WriteLine("Cylinder characteristics");
+            Console.WriteLine("Radius: {0} , Height: {1}", Radius, Height);
+            Console.WriteLine("Base: {0:f} | Lateral: {1:f} | Total: {2:f} | Volume: {3:f} ", BaseArea, LateralArea, TotalArea, Volume);
+        }
+        
+    }
+}
