6b75912368772710624fe24d03ebfe0014a7619b
minh <65264453+lehoangminhfpt2020@users.noreply.github.com>
2021-01-22 10:32:57 +0700

    Cach 2

---
+++ GeometryExample/Exercise2 cach 2/Animals.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    class Animals
+    {
+        int Weight;
+        string Name;
+
+        public void SetMe(string Name, int Weight)
+        {
+            this.Name = Name;
+            this.Weight = Weight;
+        }
+        public virtual void Show()
+        {
+            Console.WriteLine("\nName: {0}, Weight: {1}",Name, Weight);
+        }
+    }
+    class Lion : Animals
+    {
+        public override void Show()
+        {
+            base.Show();
+        }
+    }
+    class Tiger : Animals
+    {
+        public override void Show()
+        {
+            base.Show();
+        }
+    }
+    
+}
---
+++ GeometryExample/Exercise2 cach 2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lion lion = new Lion();
+            Tiger tiger = new Tiger();
+            tiger.SetMe("tiger", 100);
+            tiger.Show();
+
+            lion.SetMe("lion", 200);
+            lion.Show();
+        }
+    }
+}
